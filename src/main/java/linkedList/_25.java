e3793e31ba75117e59e25000e525e7c223faa763
lihongxing <lihongxing1019@163.com>
2023-07-04 09:37:55 +0800

    链表相关

--- src/main/java/linkedList/_25.java
+++ src/main/java/linkedList/_25.java
@@ -9,54 +9,7 @@ import java.util.Scanner;
  * @Date 2023/6/20 17:32
  */
 public class _25 {
-//        public static void main(String[] args) {
-//            List<Integer> list1 = new ArrayList<>();
-//            list1.add(1);
-//            list1.add(2);
-//            list1.add(3);
-//            List<Integer> list2 = new ArrayList<>();
-//            list2.add(4);
-//            list2.add(5);
-//            list2.add(6);
-//            List<List<Integer>> input = new ArrayList<>();
-//            input.add(list1);
-//            input.add(list2);
-//            int[][] res = getArray(input);
-//            for(int i = 0;i < res.length;i++){
-//                for(int j = 0;j < res[0].length;j++){
-//                    System.out.print(res[i][j] + "   ");
-//                }
-//            }
-//        }
-        // m个大小为n的有序数组合并成一个大小为m*n的有序数组,输入是m个有序数组
-//        public static int[][] getArray(List<List<Integer>> input){
-//            int m = input.size();
-//            int n = input.get(0).size();
-//            // 返回数组
-//            int[][] res = new int[m][n];
-//            // 优先队列
-//            Queue<int[]> queue = new PriorityQueue<>((o1,o2) -> o1[0] - o2[0]);
-//            int[] index = new int[m + 1];
-//            int num = 0;
-//            while(num < m * n){
-//                int min = 0;
-//                for(int i = 1;i <= m;i++){
-//                    if(!queue.isEmpty() && queue.peek() < input.get(i).get(index[i])){
-//                        index[i] ++;
-//                        min = i;
-//                        queue.add(input.get(i).get(index[i]));
-//                    }
-//                }
-//                min = 0;
-//                int row = num / m;
-//                int col = num % n;
-//                if(!queue.isEmpty()){
-//                    res[row][col] = queue.poll();
-//                }
-//                num++;
-//            }
-//            return res;
-//        }
+
     @Test
     public void test2(){
 //        bigIntAdd("99","99");
---
+++ src/main/java/stack/_347.java
@@ -0,0 +1,9 @@
+package stack;
+
+/**
+ * @Author lihongxing
+ * @Date 2023/7/4 9:33
+ */
+public class _347 {
+    
+}
