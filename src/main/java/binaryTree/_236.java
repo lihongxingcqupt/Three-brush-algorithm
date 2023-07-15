e2694ebbf447f772983e7533932192623f8c121a
lihongxingcqupt <lihongxing1019@163.com>
2023-07-15 14:13:05 +0800

    二叉树最近公共祖先

--- src/main/java/binaryTree/_236.java
+++ src/main/java/binaryTree/_236.java
@@ -6,7 +6,19 @@ package binaryTree;
  */
 public class _236 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        return null;
+        if(root == null || root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left == null && right == null){
+            return null;
+        }else if(left == null){
+            return right;
+        }else if(right == null){
+            return left;
+        }else{
+            return root;
+        }
     }
 }
