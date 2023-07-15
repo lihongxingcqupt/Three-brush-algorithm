7b1b86401f67e7f967dfa1b7cf5b063d6dfc3e78
lihongxingcqupt <lihongxing1019@163.com>
2023-07-15 13:51:10 +0800

    二叉树最近公共祖先

---
+++ src/main/java/binaryTree/_236.java
@@ -0,0 +1,12 @@
+package binaryTree;
+
+/**
+ * @Author lihongxing
+ * @Date 2023/7/15 13:50
+ */
+public class _236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+}
