e20f02e6cbbd3fb42f123dc311655c304e89f0d2
lihongxingcqupt <lihongxing1019@163.com>
2023-08-02 00:05:07 +0800

    测试权限

---
+++ src/main/java/avarice/test1.java
@@ -0,0 +1,11 @@
+package avarice;
+
+/**
+ * @Author lihongxing
+ * @Date 2023/8/1 23:58
+ */
+public class test1 {
+    public void test(){
+        System.out.println(1);
+    }
+}
---
+++ src/main/java/avarice/test2.java
@@ -0,0 +1,17 @@
+package avarice;
+
+/**
+ * @Author lihongxing
+ * @Date 2023/8/1 23:59
+ */
+public class test2 extends test1{
+    public static void main(String[] args) {
+        test1 t = new test1();
+
+    }
+    @Override
+    public void test() {
+        System.out.println(1);
+    }
+
+}
